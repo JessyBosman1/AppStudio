ea2cfe7cb950889e99d57a16515250c8dada7c80
Jessy Bosman <je.bo@live.nl>
2017-11-01 13:36:18 +0100

    + Added save states + Added image save states while rotating

--- Mr_Potato_Head/app/src/main/java/com/example/jebo/mr_potato_head/MainActivity.java
+++ Mr_Potato_Head/app/src/main/java/com/example/jebo/mr_potato_head/MainActivity.java
@@ -6,11 +6,103 @@ import android.view.View;
 import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("BoolHat", findViewById(R.id.imageHat).getVisibility());
+        outState.putInt("BoolGlasses", findViewById(R.id.imageGlasses).getVisibility());
+        outState.putInt("BoolEyes", findViewById(R.id.imageEyes).getVisibility());
+        outState.putInt("BoolArms", findViewById(R.id.imageArms).getVisibility());
+        outState.putInt("BoolMouth", findViewById(R.id.imageMouth).getVisibility());
+        outState.putInt("BoolEyebrows", findViewById(R.id.imageBrows).getVisibility());
+        outState.putInt("BoolEars", findViewById(R.id.imageEars).getVisibility());
+        outState.putInt("BoolNose", findViewById(R.id.imageNose).getVisibility());
+        outState.putInt("BoolMoustache", findViewById(R.id.imageMoustache).getVisibility());
+        outState.putInt("BoolShoes", findViewById(R.id.imageShoes).getVisibility());
+    }
+
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        if (savedInstanceState.getInt("BoolHat") == 0){
+            findViewById(R.id.imageHat).setVisibility(View.VISIBLE);
+        }
+        else{
+            findViewById(R.id.imageHat).setVisibility(View.INVISIBLE);
+        }
+
+        if (savedInstanceState.getInt("BoolGlasses") == 0){
+            findViewById(R.id.imageGlasses).setVisibility(View.VISIBLE);
+        }
+        else{
+            findViewById(R.id.imageGlasses).setVisibility(View.INVISIBLE);
+        }
+
+        if (savedInstanceState.getInt("BoolEyes") == 0){
+            findViewById(R.id.imageEyes).setVisibility(View.VISIBLE);
+        }
+        else{
+            findViewById(R.id.imageEyes).setVisibility(View.INVISIBLE);
+        }
+
+        if (savedInstanceState.getInt("BoolArms") == 0){
+            findViewById(R.id.imageArms).setVisibility(View.VISIBLE);
+        }
+        else{
+            findViewById(R.id.imageArms).setVisibility(View.INVISIBLE);
+        }
+
+        if (savedInstanceState.getInt("BoolMouth") == 0){
+            findViewById(R.id.imageMouth).setVisibility(View.VISIBLE);
+        }
+        else{
+            findViewById(R.id.imageMouth).setVisibility(View.INVISIBLE);
+        }
+
+        if (savedInstanceState.getInt("BoolEyebrows") == 0){
+            findViewById(R.id.imageBrows).setVisibility(View.VISIBLE);
+        }
+        else{
+            findViewById(R.id.imageBrows).setVisibility(View.INVISIBLE);
+        }
+
+        if (savedInstanceState.getInt("BoolEars") == 0){
+            findViewById(R.id.imageEars).setVisibility(View.VISIBLE);
+        }
+        else{
+            findViewById(R.id.imageEars).setVisibility(View.INVISIBLE);
+        }
+
+        if (savedInstanceState.getInt("BoolNose") == 0){
+            findViewById(R.id.imageNose).setVisibility(View.VISIBLE);
+        }
+        else{
+            findViewById(R.id.imageNose).setVisibility(View.INVISIBLE);
+        }
+
+        if (savedInstanceState.getInt("BoolMoustache") == 0){
+            findViewById(R.id.imageMoustache).setVisibility(View.VISIBLE);
+        }
+        else{
+            findViewById(R.id.imageMoustache).setVisibility(View.INVISIBLE);
+        }
+
+        if (savedInstanceState.getInt("BoolShoes") == 0){
+            findViewById(R.id.imageShoes).setVisibility(View.VISIBLE);
+        }
+        else{
+            findViewById(R.id.imageShoes).setVisibility(View.INVISIBLE);
+        }
+
     }
 
     public void checkBoxClick(View view) {
