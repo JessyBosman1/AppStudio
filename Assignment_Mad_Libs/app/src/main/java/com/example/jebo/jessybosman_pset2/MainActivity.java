51b4b5458dff0608c96964b7c7157c4d5e2e8091
Jessy Bosman <je.bo@live.nl>
2017-11-09 16:00:49 +0100

    Finished

--- Assignment_Mad_Libs/app/src/main/java/com/example/jebo/jessybosman_pset2/MainActivity.java
+++ Assignment_Mad_Libs/app/src/main/java/com/example/jebo/jessybosman_pset2/MainActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.InputStream;
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity {
+    Story story;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,8 +17,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToQuestions(View view) {
+        Random rand = new Random();
+        int n = rand.nextInt(5);
+        if(n==0){
+            InputStream stream = this.getResources().openRawResource(R.raw.madlib0_simple);
+            story = new Story(stream);}
+        if(n==1){
+            InputStream stream = this.getResources().openRawResource(R.raw.madlib1_tarzan);
+            story = new Story(stream);}
+        if(n==3){
+            InputStream stream = this.getResources().openRawResource(R.raw.madlib2_university) ;
+            story = new Story(stream);}
+        if(n==4){
+            InputStream stream = this.getResources().openRawResource(R.raw.madlib4_dance);
+            story = new Story(stream);}
+
        Intent intent = new Intent(this, QuestionActivity.class);
+       intent.putExtra("Story", story);
        startActivity(intent);
-        finish();
     }
 }
