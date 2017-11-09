7c88771ef9c21a5f2ce01eef06cce602297e75d5
Jessy Bosman <je.bo@live.nl>
2017-11-09 13:24:07 +0100

    added questions placeholders and code

---
+++ Assignment_Mad_Libs/app/src/main/java/com/example/jebo/jessybosman_pset2/Activity_Result.java
@@ -0,0 +1,13 @@
+package com.example.jebo.jessybosman_pset2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__result);
+    }
+}
